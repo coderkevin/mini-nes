abc2c36ed0e59f2ff6eb6de50fa3f8a0b12b16ab
Kevin Killingsworth <kevin@redfenix.com>
2017-04-23 22:22:13 -0500

    nfc: Add base nfc init code

--- nfc/nfc_get_uid.c
+++ nfc/nfc_get_uid.c
@@ -6,6 +6,15 @@
 #include <nfc/nfc.h>
 #include <nfc/nfc-types.h>
 
+#include "utils/nfc-utils.h"
+
+// Modulations to scan for
+const nfc_modulation nmModulations[2] = {
+	{ .nmt = NMT_ISO14443A, .nbr = NBR_106 },
+	{ .nmt = NMT_ISO14443B, .nbr = NBR_106 },
+};
+const size_t szModulations = 5;
+
 static nfc_device *pnd = NULL;
 static nfc_context *context;
 
@@ -36,7 +45,10 @@ int main( int argc, const char *argv[] )
 	// Display libnfc version
 	const char *libnfcVersion = nfc_version();
 
-	printf( "%s uses libnfc %s\n", argv[0], libnfcVersion );
+	if ( verbose ) {
+		printf( "%s uses libnfc %s\n", argv[0], libnfcVersion );
+	}
+
 	if ( argc != 1 ) {
 		if ( ( argc = 2 ) && ( 0 == strcmp( "-v", argv[1] ) ) ) {
 			verbose = true;
@@ -45,5 +57,41 @@ int main( int argc, const char *argv[] )
 			exit( EXIT_FAILURE );
 		}
 	}
+
+	nfc_target nt;
+	int res = 0;
+
+	nfc_init( &context );
+	if ( NULL == context ) {
+		ERR( "Unable to init libnfc (malloc)" );
+		exit( EXIT_FAILURE );
+	}
+
+	pnd = nfc_open( context, NULL );
+
+	if ( NULL == pnd ) {
+		ERR( "%s", "Unable to open NFC device." );
+		nfc_exit( context );
+		exit( EXIT_FAILURE );
+	}
+
+	if ( nfc_initiator_init( pnd ) < 0 ) {
+		nfc_perror( pnd, "nfc_initiator_init" );
+		nfc_close( pnd );
+		nfc_exit( context );
+		exit( EXIT_FAILURE );
+	}
+
+	if ( verbose ) {
+		printf( "NFC reader: %s opened\n", nfc_device_get_name( pnd ) );
+	}
+
+	nfc_close( pnd );
+	nfc_exit( context );
+
+	if ( verbose ) {
+		printf( "NFC reader: %s closed\n", nfc_device_get_name( pnd ) );
+	}
+	exit( EXIT_SUCCESS );
 }
 
