5b4ab406031fd20cac98bb9a985088eccb2dfba7
Kevin Killingsworth <kevin@redfenix.com>
2017-05-14 10:51:25 -0500

    Add daemon for nes_buttons

--- buttons/nes_buttons_daemon.py
+++ buttons/nes_buttons_daemon.py
@@ -8,7 +8,7 @@ import daemon
 from daemon import pidfile
 import log
 import logging
-import nfc_poll
+import nes_buttons
 
 debug_p = True
 
@@ -18,12 +18,12 @@ def start_daemon( pidf, logf ):
     global debug_p
 
     if debug_p:
-        print( "nfc_poll_daemon: entered start_daemon()")
-        print( "nfc_poll_daemon: pidf={}  logf={}".format( pidf, logf ) )
+        print( "nes_buttons_daemon: entered start_daemon()" )
+        print( "nes_buttons_daemon: pidf={} logf={}".format( pidf, logf ) )
 
     try:
         with daemon.DaemonContext(
-            working_directory='/var/lib/nfc_poll',
+            working_directory='/var/lib/nes_buttons',
             gid = grp.getgrnam( 'nes' ).gr_gid,
             umask=0o002,
             pidfile=pidfile.TimeoutPIDLockFile( pidf ),
@@ -33,31 +33,24 @@ def start_daemon( pidf, logf ):
             logFormat = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 
             logger = log.initLogger( logLevel, logFormat, logf )
-            logger.info( "nfc_poll_daemon: entered daemon context" )
+            logger.info( "nes_buttons_daemon: entered daemon context" )
 
             try:
-                options = nfc_poll.initConfig( logger )
-                logger.info( "nfc_poll_daemon: options: {}".format( options ) )
-
-                app = nfc_poll.NFCPoll( options, logger )
-                context.signal_map = {
-                    signal.SIGTERM: app.cleanup
-                    }
-                logger.info( "nfc_poll_daemon: running app" )
+                app = nes_buttons.NESButtons( logger )
+                logger.info( "nes_buttons_daemon: running app" )
                 app.run()
             except:
                 logger.error( traceback.format_exc() )
                 sys.exit(1)
 
     except:
-        print( "Unhandled Exception: {}".format( traceback.format_exc() ) )
+        print( "Unhandled exception: {}".format( traceback.format_exc() ) )
         sys.exit(1)
 
-
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser( description="NFC Poll Daemon" )
-    parser.add_argument( '-p', '--pid-file', default='/var/run/nfc_poll.pid' )
-    parser.add_argument( '-l', '--log-file', default='/var/log/nfc_poll.log' )
+    parser = argparse.ArgumentParser( description="NES Buttons Daemon" )
+    parser.add_argument( '-p', '--pid-file', default='/var/run/nes_buttons.pid' )
+    parser.add_argument( '-l', '--log-file', default='/var/run/nes_buttons.log' )
 
     args = parser.parse_args()
 
