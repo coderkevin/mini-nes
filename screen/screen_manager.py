d7b9ec1206ceb937a6c4f308e22e4dbf9112ea4f
Kevin Killingsworth <kevin@redfenix.com>
2017-05-24 01:11:08 -0500

    Screen Manager: Add file watching

--- screen/screen_manager.py
+++ screen/screen_manager.py
@@ -6,6 +6,8 @@ import log
 import logging
 import argparse
 import ConfigParser
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
 
 logLevel = logging.DEBUG;
 logFormat = '%(asctime)s - %(levelname)s - %(message)s'
@@ -18,16 +20,21 @@ DEFAULT_ACTION = { 'type': 'dashboard' }
 # Action functions
 
 def run_rom( options ):
-    system = options[ 'system' ]
-    path = options[ 'path' ]
+    if not 'system' in options:
+        logger.error( "Expected 'system' option" )
+    elif not 'path' in options:
+        logger.error( "Expected 'path' option" )
+    else:
+        system = options[ 'system' ]
+        path = options[ 'path' ]
 
-    logger.info( 'Running rom: {}'.format( path ) )
-    cmd = "{} 0 _SYS_ {} '{}'".format( runcommand, system, path )
-    return subprocess.Popen( shlex.split( cmd ) )
+        logger.info( 'Running rom: {}'.format( path ) )
+        cmd = "{} 0 _SYS_ {} '{}'".format( RUNCOMMAND, system, path )
+        return cmd
 
 def run_dashboard( options ):
     logger.info( "Running dashboard" )
-    return subprocess.Popen( EMULATIONSTATION )
+    return EMULATIONSTATION
 
 ACTIONS = {
     'dashboard': run_dashboard,   # Expects no options
@@ -41,34 +48,77 @@ def terminate_process( process ):
         child.terminate()
     process.terminate()
 
+class ConfigChangeHandler( FileSystemEventHandler ):
+    def __init__( self, screen_manager ):
+        self.screen_manager = screen_manager
+        self.config_file = screen_manager.config_file
+
+    def on_created( self, event ):
+        if ( event.src_path == self.config_file ):
+            logger.debug( "Config file created: {}".format( event ) )
+            self.screen_manager.read_config()
+
+    def on_modified( self, event ):
+        if ( event.src_path == self.config_file ):
+            logger.debug( "Config file modified: {}".format( event ) )
+            self.screen_manager.read_config()
+
+    def on_deleted( self, event ):
+        if ( event.src_path == self.config_file ):
+            logger.debug( "Config file deleted: {}".format( event ) )
+            self.screen_manager.read_config()
+
 class ScreenManager():
-    def __init__( self, configFile ):
-        self.configFile = configFile
+    def __init__( self, config_file ):
+        self.config_file = config_file
         self.lastAction = None
+        self.process = None
 
-    def read_config_action( self, configFile ):
+        change_handler = ConfigChangeHandler( self )
+        self.observer = Observer()
+        self.observer.schedule( change_handler, os.path.dirname( config_file ) )
+
+    def read_config( self ):
+        action = DEFAULT_ACTION
+
         config = ConfigParser.RawConfigParser()
-        config.read( configFile )
-
+        config.read( self.config_file )
         if config.has_section( 'Action' ):
-            return config.options( 'Action' )
+            # Construct a dictionary for the Action section
+            action = {}
+            for name in config.options( 'Action' ):
+                action[ name ] = config.get( 'Action', name )
+
+        if action != self.lastAction:
+            logger.debug( "Action: {}".format( action ) );
+            self.run_action( action )
         else:
-            return DEFAULT_ACTION
+            logger.debug( "Action is same as last action. Ignoring." );
 
     def run_action( self, action ):
         logger.debug( "Running action: {}".format( action ) )
+
         self.lastAction = action
 
         actionFunc = ACTIONS[ action[ 'type' ] ];
 
         if actionFunc:
-            actionFunc( action )
+            cmd = actionFunc( action )
+
+            if cmd:
+                if self.process:
+                    terminate_process( self.process )
+
+                logger.debug( "Running command: {}".format( cmd ) );
+                self.process = subprocess.Popen( shlex.split( cmd ) )
+
         else:
             logger.error( "Action '{}' not recognized.".format( action[ 'type' ] ) )
 
     def start( self ):
-        action = self.read_config_action( self.configFile )
-        self.run_action( action )
+        self.read_config()
+        self.observer.start()
+        self.observer.join()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser( description="Screen Manager" )
@@ -78,6 +128,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     logger = log.initLogger( logLevel, logFormat, args.log_file )
+    logger.info( "Screen Manager Start" )
 
     screenManager = ScreenManager( args.config )
     screenManager.start()
